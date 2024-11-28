0ba7759d01873857248d68e6aee4c39eb5ce7b09
rickastlee <45075899+rickastlee@users.noreply.github.com>
2024-11-28 15:53:06 +0100

    Kang build script from Velvet

--- build.sh
+++ build.sh
@@ -2,30 +2,28 @@
 
 #set -e
 
-if ! [ -d "$HOME/weebx-clang" ]; then
-echo "Clang not found!"
-git clone git@github.com:XSans0/WeebX-Clang.git "$HOME/weebx-clang"
-fi
-
 ## Copy this script inside the kernel directory
-KERNEL_DEFCONFIG=cust_defconfig
+KERNEL_DEFCONFIG=vendor/miatoll-perf_defconfig
 ANYKERNEL3_DIR=$PWD/AnyKernel3/
-FINAL_KERNEL_ZIP=YAMK-miatoll-$(date '+%Y%m%d').zip
-export PATH="$HOME/weebx-clang/bin:$PATH"
+FINAL_KERNEL_ZIP=Niggatron-NonKSU-MiAtoll-$(date '+%Y%m%d').zip
+export PATH="$HOME/tools/yuki-clang/bin:$PATH"
 export ARCH=arm64
-export KBUILD_BUILD_HOST=cosmos
-export KBUILD_BUILD_USER=cosmic
-export KBUILD_COMPILER_STRING="$($HOME/weebx-clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+export KBUILD_BUILD_HOST=Github-CI
+export KBUILD_BUILD_USER=TxExcalibur
+export KBUILD_COMPILER_STRING="$($HOME/tools/yuki-clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+
+if ! [ -d "$HOME/tools/yuki-clang" ]; then
+echo "Clang not found! Cloning..."
+if ! git clone -q https://bitbucket.org/thexperienceproject/yuki-clang.git --depth=1 --single-branch ~/tools/yuki-clang; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
+fi
 
 # Speed up build process
 MAKE="./makeparallel"
 
 BUILD_START=$(date +"%s")
-blue='\033[0;34m'
-cyan='\033[0;36m'
-yellow='\033[0;33m'
-red='\033[0;31m'
-nocol='\033[0m'
 
 # Clean build always lol
 echo "**** Cleaning ****"
@@ -34,14 +32,14 @@ make O=out clean
 make mrproper
 
 echo "**** Kernel defconfig is set to $KERNEL_DEFCONFIG ****"
-echo -e "$blue***********************************************"
-echo "          BUILDING KERNEL          "
-echo -e "***********************************************$nocol"
+echo -e "***************************************************"
+echo "****          BUILDING KERNEL          ****"
+echo -e "***************************************************"
 make $KERNEL_DEFCONFIG O=out
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
                       CC=clang \
-                      CROSS_COMPILE=aarch64-linux-gnu- \
+		      CROSS_COMPILE=aarch64-linux-gnu- \
                       CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
                       LD=ld.lld \
                       LLVM=1 \
@@ -69,17 +67,19 @@ cp $PWD/out/arch/arm64/boot/dts/qcom/cust-atoll-ab.dtb $ANYKERNEL3_DIR/dtb
 echo "**** Time to zip up! ****"
 cd $ANYKERNEL3_DIR/
 zip -r9 "../$FINAL_KERNEL_ZIP" * -x README $FINAL_KERNEL_ZIP
+MD5CHECK=$(md5sum "../$FINAL_KERNEL_ZIP" | cut -d' ' -f1)
 
-echo "**** Done, here is your sha1 ****"
+echo "**** Done, here is your MD5 ****"
 cd ..
 rm -rf $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP
 rm -rf $ANYKERNEL3_DIR/Image.gz
 rm -rf $ANYKERNEL3_DIR/dtbo.img
 rm -rf $ANYKERNEL3_DIR/dtb
 rm -rf out/
-
-sha1sum $FINAL_KERNEL_ZIP
+md5sum $FINAL_KERNEL_ZIP
 
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
-echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
+post_msg="Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s)
+MD5 Checksum: $MD5CHECK"
+curl -v -F chat_id=$chat_id -F document=@$FINAL_KERNEL_ZIP -F caption="$post_msg" https://api.telegram.org/bot$token/sendDocument
